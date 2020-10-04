8c28e9a909536fe4f1768e50e4b6be5c55a6d71e
gaurav-2626 <gauravagrawal_@outlook.com>
2020-10-05 00:28:04 +0530

    4th module

--- three-step-counter/src/App.js
+++ three-step-counter/src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./App.css" ;
 
 function App(){
+  //never change a state variable directly
+  //set count is used to change the variable
+  //initial value of count is given inside the bracket.
+  const [count,setCount] = useState(0)
+  //declaring variables.
+  //val , setVal
+
    return(
-     <div>
-       <h1> Hello ! </h1>
+     <div className = "App">
+        <header>
+            <h1>
+              Counter app using State/hooks
+            </h1>
+        </header>
+   <h2>Current value of count is {count}</h2>
+   <button onClick={() => setCount(0)}> Reset Counter</button>
+   <button onClick={() => count > 10 ? "" : setCount(count + 1)}> Increase Counter</button> 
+   <button onClick={() => count < -10 ? "" : setCount(count - 1)}> Decrease Counter</button> 
+
      </div>
    )
 }
